2aeddae96592479d6577e0e9530a17560aee04c5
UtkarshSinha121 <be20cs115utkarshsinha@gmail.com>
2023-09-15 11:04:50 +0530

    install formik yup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+import Signup from './component/Signup';
+import Login from './component/Login';
+import Home from './component/Home';
+import MyNetwork from './component/MyNetwork';
+import Jobs from './component/Jobs';
+import Message from './component/Message';
+import Notification from './component/Notification';
+import Profile from './component/Profile';
+import AddPost from './component/AddPost';
+import EditPost from './component/EditPost';
+import PostPage from './component/PostPage';
+import Search from './component/Search';
+import { Toaster } from 'react-hot-toast';
+import { UserProvider } from './UserContext';
+
+
+
+
 
 function App() {
   return (
-    <div>hello</div>
+    <div>
+    <Toaster position='top-center' />
+  <BrowserRouter>
+    <UserProvider>
+     <Routes>
+         <Route path='/' element={ <Signup/> }/>
+         <Route path='signup' element={ <Signup/> }/>
+         <Route path='login' element={ <Login/> }/>
+         <Route path='home' element={ <Home/> }/>
+         <Route path='mynetwork' element={ <MyNetwork/> }/>
+         <Route path='jobs' element={ <Jobs/> }/>
+         <Route path='message' element={ <Message/> }/>
+         <Route path='notification' element={ <Notification/> }/>
+         <Route path='profile' element={ <Profile/> }/>
+         <Route path='addpost' element={ <AddPost/> }/>
+         <Route path='editpost/:id' element={ <EditPost/> }/>
+         <Route path='postpage' element={ <PostPage/> }/>
+         <Route path='search'  element={ <Search/> }/>
+
+         
+        
+        
+        
+
+     </Routes>
+     </UserProvider>
+  </BrowserRouter>
+
+ </div>
   );
 }
 
