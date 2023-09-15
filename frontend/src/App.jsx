f1ff9a6547983b79800e6ad86e824875c03ea075
UtkarshSinha121 <be20cs115utkarshsinha@gmail.com>
2023-09-15 11:44:03 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,6 @@ import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
 import Signup from './component/Signup';
 import Login from './component/Login';
 import Home from './component/Home';
-import MyNetwork from './component/MyNetwork';
-import Jobs from './component/Jobs';
-import Message from './component/Message';
-import Notification from './component/Notification';
 import Profile from './component/Profile';
 import AddPost from './component/AddPost';
 import EditPost from './component/EditPost';
@@ -30,10 +26,6 @@ function App() {
          <Route path='signup' element={ <Signup/> }/>
          <Route path='login' element={ <Login/> }/>
          <Route path='home' element={ <Home/> }/>
-         <Route path='mynetwork' element={ <MyNetwork/> }/>
-         <Route path='jobs' element={ <Jobs/> }/>
-         <Route path='message' element={ <Message/> }/>
-         <Route path='notification' element={ <Notification/> }/>
          <Route path='profile' element={ <Profile/> }/>
          <Route path='addpost' element={ <AddPost/> }/>
          <Route path='editpost/:id' element={ <EditPost/> }/>
